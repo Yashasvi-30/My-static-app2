79652800e4173fac9fb19189e7e4ad424e8898b5
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-17 21:33:25 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return <div>
     <h1>Hello {value}</h1>
     <h2>This is my React Static Web Application </h2>
-    <p>Hi , My name is Yashasvi Gupta .</p>
+    <p>Hi , My name is Yashasvi Gupta . I study B-tech in Computer science majoring in AI/ML . </p>
     </div>;
 }
 
