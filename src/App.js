16cafc62e1946c42e6170a21c64255e1e664b745
Yashasvi Gupta <Yashasvi.101768@stu.upes.ac.in>
2024-06-17 21:32:05 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Hello {value}</h1>
+    <h2>This is my React Static Web Application </h2>
+    <p>Hi , My name is Yashasvi Gupta .</p>
+    </div>;
 }
 
 export default App;
